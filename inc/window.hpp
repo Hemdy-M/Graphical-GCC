1e51ce8d9cd5e6b8e661efe4b840652098b4710d
Hemdy-M <hemdy.formation@gmail.com>
2025-05-29 17:48:48 +0200

    Start from beginig

---
+++ inc/window.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <GLFW/glfw3.h>
+#include <stdexcept>
+#include <cassert>
+
+class Window {
+
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,16 @@
 #include <imgui.h>
 #include <backends/imgui_impl_glfw.h>
 #include <backends/imgui_impl_opengl3.h>
-#include <GLFW/glfw3.h>
 #include <iostream>
-#include <application.hpp>
-
-static void glfw_error_callback(int error, const char* description);
+#include <string>
+#include <window.hpp>
 
 int main(int, char**) {
-    App::InitializedGLFWAndOtherStuff(glfw_error_callback);
-    auto* win = App::CreateWindow();
-    App::SetupImGUI(win);
-    App::Update(win);
-    App::TerminateProg(win);
-}
+    // Init GLFW and window hint
+    if (!glfwInit()) {
+        std::cerr << "GLFW cannot init itself" << std::endl;
+        exit(EXIT_FAILURE);
+    }
 
-static void glfw_error_callback(int error, const char* description) {
-    std::cerr << "GLFW error : " << error << " : " << description << std::endl;
+    
 }
